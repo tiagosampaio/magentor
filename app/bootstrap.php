addfb0b0a3a87e06386a2d458c1ee875fbab9251
Tiago Sampaio <tiago@tiagosampaio.com>
2018-04-13 16:49:50 -0300

    Improvements to application.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,6 +7,32 @@ error_reporting(E_ALL);
 
 require_once ROOT . '/app/autoload.php';
 
+$templates = ROOT . '/repository/templates';
+$loader = new \Twig\Loader\FilesystemLoader($templates);
+$twig   = new \Twig\Environment($loader, [
+    // 'cache' => ROOT . '/to/compilation_cache'
+]);
+
+/** @var \Twig\TemplateWrapper $template */
+/*
+try {
+    $template = $twig->load('magento1/module/xml/declaration.twig');
+    $data = $template->render([
+        'vendor'       => 'MagedIn',
+        'module_name'  => 'Example',
+        'dependencies' => [
+            'Mage_Catalog',
+            'Mage_Core'
+        ],
+    ]);
+
+    file_put_contents(ROOT . '/declaration.xml', $data);
+
+} catch (Exception $e) {
+    $x = '';
+}
+*/
+
 \Magentor\Framework\Filesystem\DirectoryRegistrar::register(ROOT);
 
 $bootstrap = \Magentor\Framework\App\Bootstrap::create(ROOT, $_SERVER);
