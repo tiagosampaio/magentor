e7c44e72d9224d9e5c012dde76a625eb7a16c624
Tiago Sampaio <tiago@tiagosampaio.com>
2018-04-21 17:50:05 -0300

    Modifications to module load.

--- app/etc/modules.php
+++ app/etc/modules.php
@@ -1,7 +1,32 @@
 <?php
 
-return [
-    'Magentor/ModuleCreator',
-    'Magentor/MagentoInfo',
-    'Magentor/ModuleInfo',
-];
+$codeDir = \Magentor\Framework\Filesystem\DirectoryRegistrar::getCodeDir();
+$dirs    = scandir($codeDir);
+
+$modules = [];
+
+function validateDirname($dirName) {
+    if ('.' == $dirName || '..' == $dirName) {
+        return false;
+    }
+
+    return true;
+}
+
+foreach ($dirs as $vendor) {
+    if (!validateDirname($vendor)) {
+        continue;
+    }
+
+    $vendorModules = scandir($codeDir . '/' . $vendor);
+
+    foreach ($vendorModules as $module) {
+        if (!validateDirname($module)) {
+            continue;
+        }
+
+        $modules[] = implode(DIRECTORY_SEPARATOR, [$vendor, $module]);
+    }
+}
+
+return $modules;
