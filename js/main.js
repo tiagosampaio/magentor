9e0cad2ea44376a88ede36463235c204ef92f915
Tiago Sampaio <tiago@tiagosampaio.com>
2018-05-04 13:54:02 -0300

    Init page.

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// Remove preload class once page is fully loaded
+
+$(document).ready(function() {
+  $('body').removeClass('preload');
+});
+
+// Add class to navigation when scrolling down
+
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 20) {
+    $('.header-main').addClass('fade-in');
+  } else {
+    $('.header-main').removeClass('fade-in');
+  }
+});
+
+// Smooth scroll to anchor points
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top-80
+  }, 500);
+});
+
+// Add class when mobile navigation icon is clicked
+
+$('.nav-toggle').on('click', function() {
+  $('body').toggleClass('no-scroll');
+  $('.header-main').toggleClass('active');
+});
+
+// Prevent background from scrolling on mobile when navigation is toggled
+
+$('html, body').on('touchmove', function() {
+  e.preventDefault();
+});
+
+// Add class when sidebar dropdown icon is clicked
+
+$('.sidebar .sidebar-header').on('click', function() {
+  $('.sidebar').toggleClass('active');
+});
+
+// Add class when switching between monthly and yearly pricing
+
+$('#yearly').on('click', function() {
+  $('.pricing-monthly').toggleClass('inactive');
+  $('.pricing-yearly').toggleClass('active');
+});
